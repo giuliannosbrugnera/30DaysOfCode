45e954199df47d228a87b7de48bed3e7b299cb8c
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-01-15 10:16:03 -0600

    Add day 14 solution

---
+++ day14-difference.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+
+
+    Difference( int[] elements ) {
+        this.elements = elements;
+    }
+
+    public void computeDifference() {
+        Arrays.sort( elements );
+        maximumDifference = elements[ elements.length - 1 ] - elements[ 0 ];
+    }
+} // End of Difference Class
+
+public class Solution {        
+  	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	int[] a = new int[N];
+    
+    	for (int i = 0; i < N; i++) {
+    		a[i] = sc.nextInt();
+    	}
+
+      	Difference D = new Difference(a);
+
+        D.computeDifference();
+
+        System.out.print(D.maximumDifference);
+   }
+}
